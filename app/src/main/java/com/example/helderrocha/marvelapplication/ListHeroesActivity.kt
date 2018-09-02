c99592395e287c30e9899f12a2cfcb9f5bdedcaf
Heder Rocha <hedie.strokes@gmail.com>
2018-09-02 12:53:04 -0300

    initial commit

---
+++ app/src/main/java/com/example/helderrocha/marvelapplication/ListHeroesActivity.kt
@@ -0,0 +1,12 @@
+package com.example.helderrocha.marvelapplication
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ListHeroesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.list_heroes_activity)
+    }
+}
